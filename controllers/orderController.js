f513585f2b5f6f64d6a9af04aa6ba874749627a5
ash7676 <akshaystech2233@gmail.com>
2025-09-26 18:19:54 +0530

    FEAT: changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,12 +1,18 @@
 
 const placeOrder =  function(req,res,next){
     try{
+        //user adds items to his/her cart
+        //user places order for items
+        //if order placed successfully, empty the cart
+        //if order failed,
         
     }catch(err){
         next(err)
     }
 }
 
+
+//NEED AN ORDER REVIEW API TO SHOW TOTAL PRICE, QUANTITY, TAX ,OTHER CHARGES
 const viewOrder =  function(req,res,next){
     try{
 
