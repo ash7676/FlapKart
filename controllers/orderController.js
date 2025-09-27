940f5e554baf48bfc3943456244e40d2041f3dd0
ash7676 <akshaystech2233@gmail.com>
2025-09-27 14:41:18 +0530

    FEAT: order placing api implemented

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,35 +1,54 @@
 const { default: mongoose } = require("mongoose");
 const models = require("../models");
-const { successResponse } = require("../utils/response");
+const { successResponse, errorResponse } = require("../utils/response");
+const orderConstants = require("../constants/orderConstants");
 
 const placeOrder = async function (req, res, next) {
   try {
-    console.log("Req----", req);
-    //user adds items to his/her cart
-    //user places order for items -> status pending
-
-    // proceeds to payment, if payment succed-> webhook updates order with status "paid"
-    //if payment fail-> keep order status as -> "pending"
-
-    //if payment placed successfully, empty the cart
-    //if payment failed, keep items in cart
-
     const userId = req.user?.id;
     const cartId = req.body?.cartId;
+
+    //check if cartid is a valid object id
+    if (!mongoose.Types.ObjectId.isValid(cartId))
+      throw new Error("invalid cart id");
+
     const cart = await models.carts
       .findOne({
         _id: new mongoose.Types.ObjectId(cartId),
         userId: new mongoose.Types.ObjectId(userId),
       })
       .populate("items.productId");
-      let totalOrderAmount = 0;
+
+    //if no cart exists
+    if (!cart) return errorResponse(res, "cart not found!", {});
+    if (!cart.items.length) return errorResponse(res, "cart is empty!", {});
+    let totalOrderAmount = 0;
+
+    //check if the user has a pending order, if yes throw response
+    const hasPendingOrder = await models.orders.findOne({
+      status: orderConstants.PENDING,
+      userId: new mongoose.Types.ObjectId(userId),
+    });
+    if (hasPendingOrder)
+      throw new Error(
+        `the user has a pending order. orderId:${
+          hasPendingOrder.toObject()._id
+        }`
+      );
+
     const cartItems = cart?.items.map((item) => {
       const prodId = item.productId._id;
       const name = item.productId.name;
-      const price = item.productId.price;
+      const price = item.productId?.price;
+      if (!price) {
+        return errorResponse(
+          res,
+          `price is missing for item : ${item.productId._id}`
+        );
+      }
       const quantity = item.quantity;
-      const total =  price* quantity;
-      totalOrderAmount =  totalOrderAmount + total;
+      const total = price * quantity;
+      totalOrderAmount = totalOrderAmount + total;
       return {
         productId: prodId,
         name,
@@ -37,13 +56,18 @@ const placeOrder = async function (req, res, next) {
         quantity,
       };
     });
-    const newOrder =  await models.orders.create({
-        userId,
-        items: cartItems,
-        totalAmount: totalOrderAmount,
-        status: 'pending',
-    })
-    if(newOrder) return successResponse(res,"order created",newOrder.toObject())
+
+    const newOrder = await models.orders.create({
+      userId,
+      items: cartItems,
+      totalAmount: totalOrderAmount,
+      status: orderConstants.PENDING,
+    });
+    if (newOrder) {
+      return successResponse(res, "order created", newOrder.toObject());
+    } else {
+      return errorResponse(res, "failed to create order", {});
+    }
   } catch (err) {
     next(err);
   }
